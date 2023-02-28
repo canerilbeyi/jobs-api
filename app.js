ba1c41baf6c12b50ff00212e49fcab7ae4da0371
Caner İlbeyi <caner.ilbeyi@bilgesgt.com>
2023-02-28 10:23:53 +0300

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
@@ -35,9 +40,10 @@ app.use(xss());
 // extra packages
 
 app.get("/", (req, res) => {
-  return res.send("<h1>jobs api</h1>");
-  //test
+  return res.send(`<h1>jobs api</h1> <a href="/api-docs">Documentation</a>`);
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
